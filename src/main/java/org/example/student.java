d49c37f777a5c62ac2b9a3ea4dda7e19ddfcb6e7
nbrezinova <5395380@upjs.sk>
2024-09-24 10:47:46 +0200

    oprava

--- src/main/java/org/example/student.java
+++ src/main/java/org/example/student.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+package org.example;
+
+import java.util.*;
 
 public class student {
+    private long id;
+    private String name;
+    private Gender gender;
+    private int yearOfStudy;
+    private Set<student> subject;
+
 }
